9bad2f8d37d1b2a80383d2eb93e630a24c0a87e6
loayelden <loayeldin123@gmail.com>
2024-12-30 00:47:32 +0200

    deploy

--- app/_utils/axiosClient.js
+++ app/_utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
-const apiUrl = "http://localhost:1337/api";
-// const apiUrl = "https://e-commerce-strapi-next.onrender.com/api/";
+// const apiUrl = "http://localhost:1337/api";
+const apiUrl = "https://e-commerce-strapi-next.onrender.com/api/";
 export const hostName = 'http://localhost:1337' 
 const axiosClient = axios.create({
     baseURL:apiUrl,
